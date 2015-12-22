778264f8aa652f1acd0bf684103bb7ccb076bb41
BorisMishunin <mbaforever@gmail.com>
2015-12-22 16:29:42 +0200

    Edit auth

--- web/views.py
+++ web/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, render_to_response
 from authsys.forms import AuthForm
 from authsys.models import UserSettings
 from django.template.context_processors import csrf
+from web.models import PaymentsType
 
 def index(request, form=None):
     if not form:
@@ -9,6 +10,7 @@ def index(request, form=None):
     params = {}
     params.update(csrf(request))
     params['form'] = form
+    params['range'] = range(3)
     params['user'] = request.user
     user_icon = ''
     try:
@@ -17,4 +19,5 @@ def index(request, form=None):
     except:
         pass
     params['user_icon'] = user_icon;
+    params['payment_types'] = PaymentsType.objects.all()
     return render_to_response('web/index.html', params)
